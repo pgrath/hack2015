3e45ef22fdfc0e2e786b142bf365705d239b44af
ANTHONY <ANTHONY>
2015-11-07 23:29:11 -0500

    Action Bar Implemented

--- app/src/main/java/com/example/pat/hack2015/ChatActivity.java
+++ app/src/main/java/com/example/pat/hack2015/ChatActivity.java
@@ -1,22 +1,54 @@
 package com.example.pat.hack2015;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class ChatActivity extends AppCompatActivity {
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
 
     }
+
+    public void onOption1(MenuItem menu){
+
+    }
+    public void onOption2(MenuItem menu){
+
+    }
+
+    public void onOption3(MenuItem menu){
+
+    }
+
 
 }
