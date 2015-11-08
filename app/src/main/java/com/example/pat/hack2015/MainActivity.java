06f6542316a0c9e7683569ccb97fb1a0e5b90efb
ANTHONY <ANTHONY>
2015-11-07 20:18:24 -0500

    Updated UI for other activities

--- app/src/main/java/com/example/pat/hack2015/MainActivity.java
+++ app/src/main/java/com/example/pat/hack2015/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.pat.hack2015;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,4 +41,12 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+    public void joinChat(View view){
+
+        EditText editText = (EditText)findViewById(R.id.editText);
+        startActivity(new Intent(this, ChatActivity.class));
+
+
+    }
+
 }
